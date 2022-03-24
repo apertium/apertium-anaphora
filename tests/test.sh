c23564bc5c638dd4fb43374bb68f4bddf9f381d2
Daniel Swanson <awesomeevildudes@gmail.com>
2022-03-23 23:49:25 -0400

    test to ensure that empty arx file works
    @nlhowell is annoyingly correct

--- tests/test.sh
+++ tests/test.sh
@@ -6,7 +6,11 @@ ANY_FAIL=0
 for I in *.in
 do
 	B=$(basename "$I" .in)
-	cat "$B.in" | $1 apertium-eng-spa.spa-eng.arx > "$B.tmp"
+	ARX=apertium-eng-spa.spa-eng.arx
+	if [[ $B == "empty" ]] ; then
+		ARX=empty.arx
+	fi
+	cat "$B.in" | $1 $ARX > "$B.tmp"
 	diff -wb "$B.tmp" "$B.out" > "$B.diff"
 	if [[ $? -eq 0 ]] ; then
 		echo "SUCCESS $B"
